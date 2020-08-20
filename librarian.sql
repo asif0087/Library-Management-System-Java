08f7a89523aea37b9a351bdbdb9e4955a230f3e6
A.S.M ASIF-UL-HASAN <54295053+asif0087@users.noreply.github.com>
2020-08-20 07:59:06 +0600

    Add files via upload

---
+++ librarian.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 19, 2016 at 07:40 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.4.0beta2-dev
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+CREATE TABLE IF NOT EXISTS `librarian` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `librarian`
+--
+
+INSERT INTO `librarian` (`id`, `name`, `password`, `email`, `address`, `city`, `contact`) VALUES
+(1, 'ok1', 'ppp', 'ok1r@gmail.com', 'okk', 'okkk', '12345'),
+(4, 'ok2', 'xxx', 'ok2@gmail.com', 'okk', 'okkkk', '123456'),
+(6, 'ok3', 'zzz', 'ok3@gmail.com', 'okk', 'okkkk', '1234567');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
